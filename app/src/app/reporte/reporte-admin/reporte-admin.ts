170b3ddb7d332dccd87c3736b6acf7227f21d8ab
AngelVilR <gabrielvr717@gmail.com>
2026-07-14 10:59:57 -0600

    Creación de component reporte-admin

---
+++ app/src/app/reporte/reporte-admin/reporte-admin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-admin',
+  standalone: false,
+  templateUrl: './reporte-admin.html',
+  styleUrl: './reporte-admin.css',
+})
+export class ReporteAdmin {}
--- app/src/app/reporte/reporte-module.ts
+++ app/src/app/reporte/reporte-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReporteRoutingModule } from './reporte-routing-module';
+import { ReporteAdmin } from './reporte-admin/reporte-admin';
 
 @NgModule({
-  declarations: [],
+  declarations: [ReporteAdmin],
   imports: [CommonModule, ReporteRoutingModule],
 })
 export class ReporteModule {}
